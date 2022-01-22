b0825859c5f278ca02ef38a159d607f396d40b8e
SupaDupaGuy <70619636+SupaDupaGuy@users.noreply.github.com>
2022-01-22 03:36:25 +0000

    Fixed Undefined vars errors. Only Import modules as

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import sys
 import asyncio
 from replit import db
 from keep_alive import keep_alive
-from helperFunctions.utilFunctions import *
-from helperFunctions.initFunctions import *
-from commands.mainCommands import *
+from helperFunctions import utilFunctions as util
+from helperFunctions import initFunctions as inits
+from commands import mainCommands as mc
 
 # Global Variables
 version = discord.__version__
@@ -14,7 +14,7 @@ client = discord.Client()
 server = discord.Guild
 helper = discord.utils
 
-sayHi()
+
 #moderatorRoles = RoleList()
 #moderatorRoles.roles.append('Hi')
 #moderatorRoles.roles.pop()
@@ -38,8 +38,8 @@ async def on_message(message):
     return
 
   # Commands
-  userCommands(msg, bot_symbol)
-  moderatorCommands(msg, bot_symbol, moderators)
+  #mc.userCommands(msg, bot_symbol)
+  #mc.moderatorCommands(msg, bot_symbol, moderators)
 
 
 keep_alive()
